ddec73d91b318f17847fbf4f8506ff326e253a9c
novenoid <51094996+novenoid@users.noreply.github.com>
2021-04-08 18:47:49 +0300

    this version compiles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Button} from './Button';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
